6558ae971fd0fc7811f371f0a733e01d2ea468c1
josue677 <josuetv13@yahoo.com>
2022-08-01 17:55:19 -0700

    added the rest

---
+++ wrong/kosmos/__init__.py
@@ -0,0 +1,7 @@
+from .fluxcal import *  
+from .flatfield import *
+from .apextract import *
+from .identify import *
+from .imtools import *
+from .wrappers import *
+from .version import __version__
