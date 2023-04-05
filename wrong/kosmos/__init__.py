9f212dd53bb3d5a93122e301e3d9b0729da90ae5
josue677 <josuetv13@yahoo.com>
2023-04-05 13:52:28 -0700

    why tho

--- wrong/kosmos/__init__.py
+++
@@ -1,7 +0,0 @@
-from .fluxcal import *  
-from .flatfield import *
-from .apextract import *
-from .identify import *
-from .imtools import *
-from .wrappers import *
-from .version import __version__
